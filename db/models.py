2a510dd9ed0b07940a3964b42d101561feed8076
wizmon-2 <supermit4u@protonmail.com>
2023-02-19 03:24:57 +0530

    groups model

--- db/models.py
+++ db/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Groups(models.Model):
+    id = models.IntegerField(unique=True, primary_key=True)
+
 class Todo(models.Model):
-    group = models.IntegerField()
+    group = models.ForeignKey(Groups, on_delete=models.CASCADE)
 
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255, default="")
